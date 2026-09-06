c14caa02a9ad0405ec12796a9fe2d061dc4b1f23
karla <kar.mirandac@duocuc.cl>
2026-09-06 20:56:08 -0300

    Se actualiza validaciones

--- scoutball_/js/validaciones.js
+++ scoutball_/js/validaciones.js
@@ -19,19 +19,37 @@
 
       // Obtener valores
       const nombre = document.getElementById('nombre').value.trim();
-      const email = document.getElementById('email').value.trim();
+      const pais = document.getElementById('pais').value.trim();
+      const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+      const posicion = document.getElementById('posicion').value;
+      const peso = document.getElementById('peso').value;
+      const altura = document.getElementById('altura').value;
+      const sexo = document.getElementById('sexo').value;
+      const piernaHabil = document.getElementById('piernaHabil').value;
+      const numeroTelefonico = document.getElementById('numeroTelefonico').value.trim();
+      const direccion = document.getElementById('direccion').value.trim();
+      const correoElectronico = document.getElementById('correoElectronico').value.trim();
       const password = document.getElementById('password').value;
       const confirmar = document.getElementById('confirmar').value;
-      const pais = document.getElementById('pais').value.trim();
+      const biografia = document.getElementById('biografia').value.trim();
       const terminos = document.getElementById('terminos').checked;
 
       // Reset de mensajes
       setError('err-nombre', '');
-      setError('err-email', '');
+      setError('err-pais', '');
+      setError('err-fechaNacimiento', '');
+      setError('err-posicion', '');
+      setError('err-peso', '');
+      setError('err-altura', '');
+      setError('err-sexo', '');
+      setError('err-piernaHabil', '');
+      setError('err-numeroTelefonico', '');
+      setError('err-direccion', '');
+      setError('err-correoElectronico', '');
       setError('err-password', '');
       setError('err-confirmar', '');
-      setError('err-pais', '');
       setError('err-terminos', '');
+      setError('err-biografia', '');
 
       let ok = true;
 
@@ -39,9 +57,45 @@
         setError('err-nombre', 'Ingresa tu nombre (mínimo 3 caracteres).');
         ok = false;
       }
+        if (!pais) {
+        setError('err-pais', 'Selecciona o escribe tu país.');
+        ok = false;
+      }
+      if (!fechaNacimiento) {
+        setError('err-fechaNacimiento', 'Ingresa tu fecha de nacimiento.');
+        ok = false;
+      }
 
-      if (!email.includes('@') || !email.includes('.')) {
-        setError('err-email', 'Ingresa un correo válido (ej: nombre@dominio.com).');
+      if (!posicion) {
+        setError('err-posicion', 'Selecciona tu posición.');
+        ok = false;
+      }
+      if (!peso) {
+        setError('err-peso', 'Ingresa tu peso.');
+        ok = false;
+      }
+      if (!altura) {
+        setError('err-altura', 'Ingresa tu altura.');
+        ok = false;
+      }
+      if (!sexo) {
+        setError('err-sexo', 'Selecciona tu sexo.');
+        ok = false;
+      }
+      if (!piernaHabil) {
+        setError('err-piernaHabil', 'Selecciona tu pierna hábil.');
+        ok = false;
+      }
+      if (!numeroTelefonico) {
+        setError('err-numeroTelefonico', 'Ingresa tu número telefónico.');
+        ok = false;
+      }
+      if (!direccion) {
+        setError('err-direccion', 'Ingresa tu dirección.');
+        ok = false;
+      }
+      if (!correoElectronico.includes('@') || !correoElectronico.includes('.')) {
+        setError('err-correoElectronico', 'Ingresa un correo válido (ej: nombre@dominio.com).');
         ok = false;
       }
 
@@ -55,8 +109,8 @@
         ok = false;
       }
 
-      if (!pais) {
-        setError('err-pais', 'Selecciona o escribe tu país.');
+      if (biografia.length < 10) {
+        setError('err-biografia', 'La biografía debe tener al menos 10 caracteres.');
         ok = false;
       }
 
@@ -78,14 +132,14 @@
     formLogin.addEventListener('submit', (e) => {
       e.preventDefault();
 
-      const email = document.getElementById('loginEmail').value.trim();
+      const correoElectronico= document.getElementById('loginEmail').value.trim();
       const pass = document.getElementById('loginPassword').value;
 
       setError('err-login-email', '');
       setError('err-login-password', '');
 
       let ok = true;
-      if (!email.includes('@') || !email.includes('.')) {
+      if (!correoElectronico.includes('@') || !correoElectronico.includes('.')) {
         setError('err-login-email', 'Correo no válido.');
         ok = false;
       }
